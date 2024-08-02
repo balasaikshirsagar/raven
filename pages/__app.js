8fcaa5cb3f97fc1a6d2931c3b96dff3a387da05d
KSHIRSAGAR BALASAI <67835463+balasaikshirsagar@users.noreply.github.com>
2024-08-02 13:32:13 +0530

    Update __app.js

--- pages/__app.js
+++ pages/__app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
