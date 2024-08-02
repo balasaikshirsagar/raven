b2260461148a590d35661fafe94702fd84e6cea4
KSHIRSAGAR BALASAI <67835463+balasaikshirsagar@users.noreply.github.com>
2024-08-02 13:42:33 +0530

    Update __app.js

--- pages/__app.js
+++ pages/__app.js
@@ -1,6 +1,5 @@
-// pages/_app.js
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '../styles/globals.css'; // Adjust path as necessary
+import 'bootstrap/dist/css/bootstrap.min.css'; // If using Bootstrap CSS
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
